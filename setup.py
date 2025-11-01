d6bdbad4f6c3b1809d806879ccba1441a2ad1466
yuribreion1 <yuribreion@gmail.com>
2025-11-01 18:51:33 +0100

    Setting new release 0.2.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="send_mail",
-    version="0.1.0",
+    version="0.2.0",
     description="Small SMTP email sender utility",
     packages=find_packages(exclude=("tests",)),
     python_requires=">=3.8",
